e0c5417164eb1d4ad7213c050b228faf756ea303
Quentin01 <quentin.buathier@epitech.eu>
2017-07-12 23:07:36 +0900

    Add the base sample

---
+++ sample_base/src/Application.cpp
@@ -0,0 +1,74 @@
+#include "Application.hpp"
+
+#include <lug/Graphics/Builder/Camera.hpp>
+#include <lug/Graphics/Builder/Light.hpp>
+#include <lug/Graphics/Builder/Material.hpp>
+#include <lug/Graphics/Builder/Mesh.hpp>
+#include <lug/Graphics/Builder/Scene.hpp>
+#include <lug/Graphics/Renderer.hpp>
+#include <lug/Graphics/Vulkan/Renderer.hpp>
+
+Application::Application() : lug::Core::Application::Application{{"sample_base", {0, 1, 0}}} {
+    getRenderWindowInfo().windowInitInfo.title = "Sample Base";
+}
+
+bool Application::init(int argc, char* argv[]) {
+    if (!lug::Core::Application::init(argc, argv)) {
+        return false;
+    }
+
+    lug::Graphics::Renderer* renderer = _graphics.getRenderer();
+
+    // Build the scene
+    {
+        lug::Graphics::Builder::Scene sceneBuilder(*renderer);
+        sceneBuilder.setName("scene");
+
+        _scene = sceneBuilder.build();
+        if (!_scene) {
+            LUG_LOG.error("Application: Can't create the scene");
+            return false;
+        }
+    }
+
+    // Attach camera
+    {
+        lug::Graphics::Builder::Camera cameraBuilder(*renderer);
+
+        cameraBuilder.setFovY(45.0f);
+        cameraBuilder.setZNear(0.1f);
+        cameraBuilder.setZFar(100.0f);
+
+        lug::Graphics::Resource::SharedPtr<lug::Graphics::Render::Camera::Camera> camera = cameraBuilder.build();
+        if (!camera) {
+            LUG_LOG.error("Application: Can't create the camera");
+            return false;
+        }
+
+        lug::Graphics::Scene::Node* node = _scene->createSceneNode("camera");
+        _scene->getRoot().attachChild(*node);
+
+        node->attachCamera(camera);
+
+        // Attach camera to RenderView
+        {
+            auto& renderViews = _graphics.getRenderer()->getWindow()->getRenderViews();
+
+            LUG_ASSERT(renderViews.size() > 0, "There should be at least 1 render view");
+
+            renderViews[0]->attachCamera(camera);
+        }
+    }
+
+    return true;
+}
+
+void Application::onEvent(const lug::Window::Event& event) {
+    if (event.type == lug::Window::Event::Type::Close) {
+        close();
+    }
+}
+
+void Application::onFrame(const lug::System::Time&) {
+
+}
